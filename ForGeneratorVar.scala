a08582285bfbd2c22d6918c6fbddefeb542ebf6c
藪崎卓巳 <raru@syrup16g.net>
2018-11-20 22:33:17 +0900

    add var sample

--- ForGeneratorVar.scala
+++ ForGeneratorVar.scala
@@ -2,7 +2,7 @@ object DottyForGenerator extends App {
 
   for {
     arg1 <- Seq(1,2)
-    val valCompileCheck = arg1
+    var varCompileCheck = arg1
     arg2 <- Seq(3,4)
   } yield {
     println(arg1 * arg2)
